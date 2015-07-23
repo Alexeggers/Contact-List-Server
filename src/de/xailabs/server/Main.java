902607ebc5e919943067c5244eabb2534162cc16
Alex.Eggers <alex.f.eggers@gmail.com>
2015-07-23 15:06:20 +0200

    Wrote main

--- src/de/xailabs/server/Main.java
+++ src/de/xailabs/server/Main.java
@@ -1,5 +1,10 @@
 package de.xailabs.server;
 
 public class Main {
-
+	
+	public static void main(String[] args) {
+		Controller controller = new Controller();
+		ClientConnection clientConnection = new ClientConnection(13337, controller);
+		clientConnection.startConnection();
+	}
 }
