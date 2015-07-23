c7a156d684fb0be79e5beadbabf1ab086ecec41d
Alex.Eggers <alex.f.eggers@gmail.com>
2015-07-23 15:52:58 +0200

    Finished Controller

--- src/de/xailabs/server/Controller.java
+++ src/de/xailabs/server/Controller.java
@@ -1,12 +1,28 @@
 package de.xailabs.server;
 
+import java.util.List;
+
+import de.xailabs.interfaces.IContact;
+
 public class Controller {
 	
 	private SQL sql;
+	private List<IContact> contacts;
 	
-	public void acceptCommand(CommandObject commandObject) {
-		if (commandObject.getCommand().equals("New Contact")) {
+	public List<IContact> acceptCommand(CommandObject commandObject) {
+		String command = commandObject.getCommand();
+		IContact contact = commandObject.getContact();
+		if (command.equals("view all contacts")) {
 			
+		} else if (command.equals("new contact")) {
+			sql.addNewContact(contact);
+		} else if (command.equals("update contact")) {
+			sql.updateContact(contact);
+		} else if (command.equals("delete contact")) {
+			sql.deleteContact(contact);
 		}
+		contacts = sql.getContacts();
+		return contacts;
 	}
+	
 }
