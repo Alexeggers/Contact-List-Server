1edd1b4f190b8998602a08790981ef3460315877
Alex.Eggers <alex.f.eggers@gmail.com>
2015-09-07 13:54:48 +0200

    Rebuilt with threads, does not work yet

--- src/de/xailabs/server/Connection.java
+++ src/de/xailabs/server/Connection.java
@@ -1,22 +1,48 @@
 package de.xailabs.server;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+
+import de.xailabs.client.CommandObject;
 
 public class Connection implements Runnable {
 
 	ServerController controller;
 	Socket socket;
-	int i;
+	ObjectInputStream in;
+	ObjectOutputStream out;
 	
 	public Connection (ServerController controller, Socket socket) {
 		this.controller = controller;
 		this.socket = socket;
+		try {
+			in = new ObjectInputStream(socket.getInputStream());
+			out = new ObjectOutputStream(socket.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		CommandObject inputCommand;
+		try {
+			while ((inputCommand = (CommandObject) in.readObject()) != null) {
+				if (inputCommand.getCommand().equals("delete contact") || inputCommand.getCommand().equals("update contact")) {
+					controller.acceptCommand(inputCommand);
+				} else {
+					Object returnObject = controller.acceptCommand(inputCommand);
+					out.writeObject(returnObject);
+					out.flush();
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/de/xailabs/server/ServerConnection.java
+++ src/de/xailabs/server/ServerConnection.java
@@ -1,12 +1,7 @@
 package de.xailabs.server;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import de.xailabs.client.CommandObject;
 
 public class ServerConnection {
 	
@@ -23,27 +18,14 @@ public class ServerConnection {
 	 * Starts up the server and waits for input when it does get connected.
 	 */
 	public void startConnection() {
-		try (
-			ServerSocket serverSocket = new ServerSocket(portNumber);
-			Socket clientSocket = serverSocket.accept();
-			ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());                   
-            ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
-		) {
-			System.out.println("Connection made");
-			CommandObject inputCommand;
-			while ((inputCommand = (CommandObject) in.readObject()) != null) {
-				if (inputCommand.getCommand().equals("delete contact") || inputCommand.getCommand().equals("update contact")) {
-					controller.acceptCommand(inputCommand);
-				} else {
-					Object returnObject = controller.acceptCommand(inputCommand);
-					out.writeObject(returnObject);
-					out.flush();
-				}
+		
+		try (ServerSocket serverSocket = new ServerSocket(portNumber);) {
+			while (true) {
+				Socket socket = serverSocket.accept();
+				Connection connection = new Connection(controller, socket);
+				Thread thread = new Thread(connection);
+				thread.start();
 			}
-		} catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                + portNumber + " or listening for a connection");
-            System.out.println(e.getMessage());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
